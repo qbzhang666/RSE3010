a21c44c986d08fa73ad15ce70cae0ff9f045048c
qbzhang666 <51216083+qbzhang666@users.noreply.github.com>
2025-04-27 09:06:28 +1000

    Update smr_streamlit.py

--- smr_streamlit.py
+++ smr_streamlit.py
@@ -225,7 +225,10 @@ styled_df = styled_df.format(precision=2)
 
 st.dataframe(styled_df, use_container_width=True)
 
-st.dataframe(styled_df, use_container_width=True)
+if intersection_records:
+    st.subheader("🧭 Intersection Orientations")
+    df_intersections = pd.DataFrame(intersection_records)
+    st.dataframe(df_intersections, use_container_width=True)
 
 buffer = io.BytesIO()
 fig.savefig(buffer, format="png")
