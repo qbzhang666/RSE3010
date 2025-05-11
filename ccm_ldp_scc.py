0b5ad66d0a65efb7f3ccededde89edebfacb62e8
qbzhang666 <51216083+qbzhang666@users.noreply.github.com>
2025-05-11 16:33:19 +1000

    Update ccm_ldp_scc.py

--- ccm_ldp_scc.py
+++ ccm_ldp_scc.py
@@ -188,7 +188,7 @@ else:
 ax2.plot(ldp_x, u_ldp * 1000, label=f"{ldp_model} LDP", lw=2)
 if install_criteria == "Distance from face":
     ax2.axvline(x_install, color='r', linestyle='--', label=f'Support @ x/r₀ = {x_install}')
-ax2.set_xlabel("Distance to Tunnel Face")
+ax2.set_xlabel("Distance to Tunnel Face [m]")
 ax2.set_ylabel("Radial Displacement [mm]")
 ax2.set_title("Longitudinal Deformation Profile")
 ax2.grid(True, color='lightgrey', alpha=0.4)
