a0f9b5a202d33d7da11b76f80d974b85f451142e
qbzhang666 <51216083+qbzhang666@users.noreply.github.com>
2025-05-12 18:59:05 +1000

    Update Generalised Hoek-Brown.py

--- Generalised Hoek-Brown.py
+++ Generalised Hoek-Brown.py
@@ -129,9 +129,7 @@ ax1.legend(loc="upper left", fontsize=9)
 ax2.plot(df['sign'], df['tau'], 'r-', lw=2,
          label=r'Hoek-Brown: $\tau = \frac{(\sigma_1 - \sigma_3) \sqrt{d\sigma_1/d\sigma_3}}{d\sigma_1/d\sigma_3 + 1}$')
 ax2.plot(x_fit, y_fit, 'k--', lw=2,
-         label=fr'Mohr-Coulomb: $\tau = c + \sigma_n \tan\phi$\n$(c = {cohesion:.2f} \ MPa, \phi = {phi_deg:.1f}^\circ)$'
-$(c = {cohesion:.2f}\ MPa,\ \phi = {phi_deg:.1f}^\circ)$')
-
+         label = fr'Mohr-Coulomb: $\tau = c + \sigma_n \tan\phi$' + \fr'  $(c = {cohesion:.2f}\ \mathrm{{MPa}},\ \phi = {phi_deg:.1f}^\circ)$'
 # Mohr Circles
 circle_centers = (circle_data.sig1 + circle_data.sig3) / 2
 circle_radii = (circle_data.sig1 - circle_data.sig3) / 2
