951d938da7bbbc863e2ac607714f011dce17eb78
qbzhang666 <51216083+qbzhang666@users.noreply.github.com>
2025-05-12 21:24:25 +1000

    Update Mohr-Coulomb.py

--- Mohr-Coulomb.py
+++ Mohr-Coulomb.py
@@ -149,8 +149,8 @@ ax1.grid(True)
 ax1.legend()
 
 # Shear-Normal Plot
-mc_label = fr"Mohr-Coulomb: $\tau = c + \sigma_n \tan\phi  (c = {cohesion:.2f} MPa, \phi = {friction_angle:.1f}^\circ)$"
-cutoff_label = f"Tensile Cut-off: $\\sigma_t = {tensile_cutoff_ratio:.2f} \\times \\sigma_c = {tensile_cutoff:.2f}\\ \\mathrm{{MPa}}$"
+mc_label = fr"Mohr-Coulomb: $\tau = c + \sigma_n \tan\phi$\n$(c = {cohesion:.2f}\ \mathrm{{MPa}},\ \phi = {friction_angle:.1f}^\circ)$"
+cutoff_label = fr"Tensile Cut-off: $\sigma_t = {tensile_cutoff_ratio:.2f} \times \sigma_c = {tensile_cutoff:.2f}\ \mathrm{{MPa}}$
 
 
 # Mohr-Coulomb (black) extended to horizontal axis
